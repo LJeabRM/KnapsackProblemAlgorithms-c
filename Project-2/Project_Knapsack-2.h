e1dcdd64467808d4cb1d0c15d0e7562fbd2d4b1a
LJeabRM <132929889+LJeabRM@users.noreply.github.com>
2024-12-11 15:34:13 +0700

    Delete pj 2

--- Project-2/Project_Knapsack-2.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJECT_KNAPSACK_2_H
-#define PROJECT_KNAPSACK_2_H
-
-#include "../Item.h"  // Include the Item structure, as it holds item data like weight, value, etc.
-
-// Function prototype for the genetic algorithm with improvement
-int project_genetic_algorithm_with_improvement(int maxWeight, Item* items, int n, int population_size, int generations, double mutation_rate, int elitism_count, int time_limit);
-
-#endif // PROJECT_KNAPSACK_2_H
